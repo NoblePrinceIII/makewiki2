07fbf29552437d772d6ffcc2496fa8d6ac5a1857
NoblePrinceii <farhan.begg@students.makeschool.com>
2019-12-04 14:22:47 -0800

    fixed errors

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -154,4 +154,4 @@ except ImportError:
 
 
 ALLOWED_HOSTS = ['localhost', 'uniqueprojectname.herokuapp.com']
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
