80d15638ebd4cff6532a1921c426d425738610be
NoblePrinceii <farhan.begg@students.makeschool.com>
2019-12-13 15:08:42 -0800

    makewiki

--- makewiki/settings.py
+++ makewiki/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = '1yct-t!2bnkgc7j59z+9cdd2k)@y+ftqor$!aya()3if^cnlo-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',fb-makewiki, '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -133,7 +133,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
 
 
 
